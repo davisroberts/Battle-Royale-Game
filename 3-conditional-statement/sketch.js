f6009b9501f2b9d508f9855c887724400d072878
davisroberts <davisroberts@icloud.com>
2019-05-13 15:19:06 -0700

    hud and main character

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -4,36 +4,73 @@ let balls = [];
 //create a variable to hold your avatar
 let me;
 
+function preload(){
+  img = loadImage('lorenzodavisshotgun.png');
+  img2 = loadImage('lorenzodavisenergyrifle.png');
+  img3 = loadImage('lorenzodavissniper.png');
+  img4 = loadImage('lorenzodavissmg.png');
+}
 
 function setup() {
-  createCanvas(500, 400);
-
-  //make one avatar called me
+  createCanvas(1500, 800);
   me = new Avatar(width/2, 300, 3);
+}
 
+function ar(){
+
+}
+
+function sniper(){
+
+}
+
+function shotgun(){
+
+}
+
+function pistol(){
+
+}
+
+function weapons(){
+  noFill();
+  push();
+  weaponbox();
+  pop();
+
+  push();
+  translate(-100,0);
+  weaponbox();
+  pop();
+
+  push();
+  translate(-200,0);
+  weaponbox();
+  pop();
+
+  push();
+  translate(-300,0);
+  weaponbox();
+  pop();
+
+}
+
+function weaponbox(){
+  strokeWeight(10);
+  rect(1390, 690, 100, 100);
 }
 
 function draw(){
 	background(220);
-
+  weapons();
+  image(img, 1400, 700, 70, 70);
+  image(img2, 1300, 700, 70, 70);
+  image(img3, 1200, 700, 70, 70);
+  image(img4, 1100, 700, 70, 70);
   me.drawMe();
   me.moveMe();
 
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
-    }
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
-	  }
-
 }
-
 //avatar class
 class Avatar {
 
@@ -44,16 +81,10 @@ class Avatar {
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("green");
+    		stroke("blue");
         strokeWeight(3);
     		fill("blue");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
+		    ellipse(this.x,this.y,30,30);
 	}
 
 	moveMe(){
@@ -64,44 +95,15 @@ class Avatar {
     if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
         this.y += this.speed;
     }
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move up by speed
+       this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+        this.x += this.speed;
+    }
 	}
-
-  die(){
-
-  }
-
-}
 
 
 //ball class from which to create new balls with similar properties.
-class Ball {
-
-	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
-		this.x = x;
-    this.y = y;
-    this.speed = speed;
-	}
-
-	// draw a ball on the screen at x,y
-	drawBall(){
-    	stroke(0);
-      strokeWeight(1);
-    	fill("red");
-		  ellipse(this.x,this.y,10,10);
-	}
-
-	//update the location of the ball, so it moves across the screen
-	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
-	}
-
-	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-    		}
-  	}
-
 }
