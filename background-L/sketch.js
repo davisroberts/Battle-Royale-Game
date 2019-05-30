ecaff27f7225a24b4b4d32d3fef77c5bce80c74e
davisroberts <49014855+davisroberts@users.noreply.github.com>
2019-05-30 13:43:16 -0700

    Update sketch.js

--- background-L/sketch.js
+++ background-L/sketch.js
@@ -1,7 +1,19 @@
 var health = 100
+let balls = [];
+let me;
+let m = 0;
+let boxfill = 0;
+
+function preload(){
+  img = loadImage('lorenzodavisshotgun.png');
+  img2 = loadImage('lorenzodavisenergyrifle.png');
+  img3 = loadImage('lorenzodavissniper.png');
+  img4 = loadImage('lorenzodavissmg.png');
+}
 
 function setup() {
   createCanvas(1500,800);
+  me = new Avatar(width/2, 300, 3);
 }
 
 function draw(){
@@ -88,19 +100,144 @@ ellipse(800,110,40,40)
 ellipse(730,666,40,40)
 ellipse(1000,480,40,40)
 
+keyPressed();
+weapons();
+image(img, 1400, 700, 70, 70);
+image(img2, 1300, 700, 70, 70);
+image(img3, 1200, 700, 70, 70);
+image(img4, 1100, 700, 70, 70);
+textSize(32);
+text(("4"), 1430, 670);
+text(("3"), 1330, 670);
+text(("2"), 1230, 670);
+text(("1"), 1130, 670);
+me.drawMe();
+me.moveMe();
 
 
-
-
-
-
-  line(mouseX, 0, mouseX, 800);
-   line(0, mouseY, 1500, mouseY);
+line(mouseX, 0, mouseX, 800);
+line(0, mouseY, 1500, mouseY);
 }
-//no clue how to fix this
-function keyPressed() {
-  if (keyCode === (69)) {
-    health = (health - 1);
-    print(health)
+
+
+function keyPressed(){
+  if (frameCount % 5 == 0) {
+    if (keyCode === 49){
+    boxfill = 1;
+    }
+    else if (keyCode === 50){
+      boxfill = 2;
+    }
+    else if (keyCode === 51){
+      boxfill = 3;
+    }
+    else if (keyCode === 52){
+      boxfill = 4;
+    }
+  }
 }
+
+function weapons(){
+
+  push();
+  if (boxfill==1){
+    m = 1000
+    weaponbox();
+    pop();
+  }
+
+  else{
+    push();
+    weaponbox();
+    pop();
+  }
+
+  push();
+  translate(-100,0);
+  if (boxfill = 2){
+    m = 1000
+    weaponbox();
+    pop();
+  }
+
+  else{
+    push();
+    translate(-100,0);
+    weaponbox();
+    pop();
+  }
+
+  push();
+  if (boxfill==3){
+    translate(-200,0);
+    m = 1000;
+    weaponbox();
+    pop();
+  }
+
+  else{
+    push();
+    translate(-200,0);
+    weaponbox();
+    pop();
+  }
+
+  push();
+  if (boxfill ==4){
+    translate(-300,0);
+    m = 1000;
+    weaponbox();
+    pop();
+  }
+
+  else{
+    push();
+    translate(-300,0);
+    weaponbox();
+    pop();
+  }
+
 }
+
+function weaponbox(){
+  noFill(),
+  strokeWeight(10);
+  rect(1390, 690, 100, 100);
+  fill(11, 36, 251, m);
+  rect(1390, 690, 100, 100);
+}
+
+class Avatar {
+
+  health = 100;
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+	drawMe(){  // draw the running person
+    		stroke("blue");
+        strokeWeight(3);
+    		fill("blue");
+		    ellipse(this.x,this.y,30,30);
+	}
+
+	moveMe(){
+    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+        this.y += this.speed;
+    }
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move up by speed
+       this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+        this.x += this.speed;
+    }
+	}
+
